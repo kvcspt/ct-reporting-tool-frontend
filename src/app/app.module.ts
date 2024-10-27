c5dfb49864dfcf14f93b051779e054f63f623ced
Patricia <kvcspt0@gmail.com>
2024-10-27 13:06:37 +0100

    feat: import angular material elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatCard} from "@angular/material/card";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {MatInput} from "@angular/material/input";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ToastrModule} from "ngx-toastr";
+import {MatOption} from "@angular/material/autocomplete";
+import {MatSelect} from "@angular/material/select";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormField,
+    MatCard,
+    ReactiveFormsModule,
+    MatButton,
+    MatInput,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       timeOut: 3000,
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
     }),
+    MatOption,
+    MatSelect,
+    MatLabel,
+    MatError,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
