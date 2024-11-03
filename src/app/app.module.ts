a84a6f9962e44142f0e880a29e10e595448479ab
Patricia <kvcspt0@gmail.com>
2024-11-03 15:29:16 +0100

    feat: import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
-import {MatCard} from "@angular/material/card";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatButton} from "@angular/material/button";
-import {MatInput} from "@angular/material/input";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {ToastrModule} from "ngx-toastr";
-import {MatOption} from "@angular/material/autocomplete";
-import {MatSelect} from "@angular/material/select";
+import { MatError, MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatCard } from "@angular/material/card";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { MatButton } from "@angular/material/button";
+import { MatInput } from "@angular/material/input";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
+import { MatOption } from "@angular/material/autocomplete";
+import { MatSelect } from "@angular/material/select";
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MatList, MatListItem} from "@angular/material/list";
+import {MatDialogContent, MatDialogTitle} from "@angular/material/dialog";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable
+} from "@angular/material/table";
 
 @NgModule({
   declarations: [
@@ -42,7 +53,22 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     MatSelect,
     MatLabel,
     MatError,
-    HttpClientModule
+    HttpClientModule,
+    MatListItem,
+    MatList,
+    FormsModule,
+    MatDialogContent,
+    MatDialogTitle,
+    MatTable,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatCell,
+    MatHeaderCell,
+    MatColumnDef,
+    MatHeaderRow,
+    MatRow,
+    MatRowDef,
+    MatHeaderRowDef
   ],
   providers: [],
   bootstrap: [AppComponent]
