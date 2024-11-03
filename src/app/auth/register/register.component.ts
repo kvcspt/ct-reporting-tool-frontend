b5ae4472fda8817508406faba174885c52ebefe6
Patricia <kvcspt0@gmail.com>
2024-11-03 15:30:25 +0100

    fix: registration fixes

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import { User } from '../model/user';
 import {AuthService} from "../auth.service";
 import {ToastrService} from "ngx-toastr";
@@ -16,12 +16,32 @@ export class RegisterComponent {
     this.registrationForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
       name: [''],
       title: [''],
-      role: ['']
-    });
+      role: ['USER']
+    }, { validators: this.matchValidator('password', 'confirmPassword') });
   }
 
+  matchValidator(controlName: string, matchingControlName: string): ValidatorFn {
+    return (abstractControl: AbstractControl) => {
+      const control = abstractControl.get(controlName);
+      const matchingControl = abstractControl.get(matchingControlName);
+
+      if (matchingControl!.errors && !matchingControl!.errors?.['confirmedValidator']) {
+        return null;
+      }
+
+      if (control!.value !== matchingControl!.value) {
+        const error = { passwordMismatch: 'Passwords do not match.' };
+        matchingControl!.setErrors(error);
+        return error;
+      } else {
+        matchingControl!.setErrors(null);
+        return null;
+      }
+    }
+  }
   onSubmit() {
     if (this.registrationForm.valid) {
       const newUser: User = new User(
@@ -30,7 +50,7 @@ export class RegisterComponent {
         this.registrationForm.value.password,
         this.registrationForm.value.name,
         this.registrationForm.value.title,
-        "USER"
+        this.registrationForm.value.role
       );
       this.authService.register(newUser).subscribe({
         next: (resp) => {
@@ -38,7 +58,7 @@ export class RegisterComponent {
           this.router.navigate(['/login']);
         },
         error: (error) => {
-          this.toastr.success("Something went wrong!");
+          this.toastr.error("Something went wrong!");
         }
       })
     }
