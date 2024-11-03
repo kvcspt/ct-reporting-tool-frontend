438b7fa23ba37982619e8d635824244e212a054b
Patricia <kvcspt0@gmail.com>
2024-11-03 15:31:12 +0100

    feat: extend navbar with user log in check and logout

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../auth/auth.service";
+import {ThemeService} from "../services/theme.service";
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html'
 })
 export class NavBarComponent {
+  constructor(public authService: AuthService, private themeService: ThemeService) {}
 
+  logout() {
+    this.authService.logout();
+  }
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
+
+  isDarkMode(): boolean {
+    return this.themeService.isDarkMode();
+  }
 }
