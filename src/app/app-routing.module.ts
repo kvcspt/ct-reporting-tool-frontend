afd06c9c99fd92cec2effe5d0f2619ce2e17bcdf
Patricia <kvcspt0@gmail.com>
2024-11-03 15:35:08 +0100

    feat: ct studies endpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./auth/login/login.component";
 import {RegisterComponent} from "./auth/register/register.component";
+import {CtStudiesComponent} from "./ct-studies/ct-studies.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'ct-studies', component: CtStudiesComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
