6f4806cf6b75099f8b2e3e253b379805a70c09e4
Patricia <kvcspt0@gmail.com>
2024-11-03 15:30:11 +0100

    feat: extend auth service with user log in check and logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "./model/user";
+import {Router} from "@angular/router";
+import {tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +10,26 @@ import {User} from "./model/user";
 export class AuthService {
   private readonly loginPath = "/api/auth/login";
   private readonly registerPath: string = "/api/auth/register";
-  constructor(private http: HttpClient) { }
+  private isAuthenticated = false;
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   login(user: User){
-    return this.http.post(this.loginPath, user);
+    return this.http.post(this.loginPath, user).pipe(
+      tap(() => this.isAuthenticated = true)
+    );
   }
 
   register(user: User){
     return  this.http.post(this.registerPath, user);
+  }
+
+  logout() {
+    this.isAuthenticated = false; // Set authenticated to false on logout
+    this.router.navigate(['/login']); // Navigate to the login page
+  }
+
+  isLoggedIn(): boolean {
+    return this.isAuthenticated; // Return the authentication status
   }
 }
