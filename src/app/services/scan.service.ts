1ff5f06199902325f120d7ce6b70be521d0206cd
Patricia <kvcspt0@gmail.com>
2024-11-03 15:33:57 +0100

    feat: scan service

---
+++ src/app/services/scan.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Scan} from "../model/Scan";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScanService {
+  private apiUrl = '/api/scans'; // Adjust to your API URL
+
+  constructor(private http: HttpClient) {}
+
+  getScans(): Observable<Scan[]> {
+    return this.http.get<Scan[]>(this.apiUrl);
+  }
+}
