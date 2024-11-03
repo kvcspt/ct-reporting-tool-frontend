2bd9dcd816f9454beb21f083c34a8b374ea471df
Patricia <kvcspt0@gmail.com>
2024-11-03 15:32:42 +0100

    feat: default dark theme and theme change option

---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  private isDarkTheme = false;
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.applyTheme();
+  }
+
+  private applyTheme() {
+      document.body.classList.remove('bg-dark', 'text-light', 'bg-light', 'text-dark');
+
+      if (this.isDarkTheme) {
+        document.body.classList.add('bg-dark', 'text-light');
+      } else {
+        document.body.classList.add('bg-light', 'text-dark');
+      }
+  }
+
+  isDarkMode(): boolean {
+    return this.isDarkTheme;
+  }
+}
