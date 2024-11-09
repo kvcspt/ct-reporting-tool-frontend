ca90252af8bf83172840493922621bedc4ad06a3
Patricia <kvcspt0@gmail.com>
2024-11-09 11:08:33 +0100

    refactor: set title in app component instead of index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'CT Reporting Tool';
+export class AppComponent implements OnInit {
+  private title = 'CT Reporting Tool';
+
+  public constructor(private titleService: Title) {}
+
+  public ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+  }
 }
