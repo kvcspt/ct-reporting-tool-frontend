f439d9ccbc9feb2c92a6da92a2bbae4e5f85f9a8
Patricia <kvcspt0@gmail.com>
2024-11-19 15:09:37 +0100

    refactor

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -6,14 +6,17 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  public constructor(private authService: AuthService) {}
+
   public intercept(
     req: HttpRequest<any>,
     next: HttpHandler,
   ): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('token');
+    const token = this.authService.getToken();
 
     if (token) {
       const clonedRequest = req.clone({
