7f57b1a9d5d8986e539b0a5fb09a4f6c5001efc6
Patricia <kvcspt0@gmail.com>
2024-11-19 14:32:52 +0100

    feat: auth service with jwt token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,46 +2,64 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './model/user';
 import { Router } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private readonly loginPath = '/api/auth/login';
+  private readonly loginPath: string = '/api/auth/login';
   private readonly registerPath: string = '/api/auth/register';
-  private isAuthenticated = false;
-  private loggedInUser: User | null | undefined;
 
   public constructor(
     private http: HttpClient,
     private router: Router,
+    private jwtHelper: JwtHelperService,
   ) {}
 
-  public login(user: User): Observable<User> {
-    return this.http.post<User>(this.loginPath, user).pipe(
-      tap((responseUser: User) => {
-        this.isAuthenticated = true;
-        this.loggedInUser = responseUser;
-      }),
-    );
+  public login(user: User): Observable<{ token: string }> {
+    return this.http.post<{ token: string }>(`${this.loginPath}`, user);
   }
 
   public register(user: User): Observable<object> {
     return this.http.post(this.registerPath, user);
   }
 
-  public logout(): void {
-    this.isAuthenticated = false;
-    this.loggedInUser = null;
-    this.router.navigate(['/login']);
-  }
-
-  public isLoggedIn(): boolean {
-    return this.isAuthenticated;
+  private getRole(): string {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      const decodedToken = this.jwtHelper.decodeToken(token);
+      return decodedToken.role;
+    }
+    return '';
   }
 
   public isAdmin(): boolean {
-    return this.loggedInUser != null && this.loggedInUser.role === 'ADMIN';
+    return this.getRole() === 'ADMIN';
+  }
+
+  //public logout(): void {
+  //  this.isAuthenticated = false;
+  //  this.loggedInUser = null;
+  //  this.router.navigate(['/login']);
+  //}
+
+  public isLoggedIn(): boolean {
+    const token = this.getToken();
+    return !!token;
+  }
+
+  public saveToken(token: string): void {
+    localStorage.setItem('jwt', token);
+  }
+
+  public getToken(): string | null {
+    return localStorage.getItem('jwt');
+  }
+
+  public logout(): void {
+    localStorage.removeItem('jwt');
+    this.router.navigate(['/login']);
   }
 }
