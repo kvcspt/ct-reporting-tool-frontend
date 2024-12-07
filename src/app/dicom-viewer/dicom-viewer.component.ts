c788df250595832f96138ca1efd5bc652accd900
Patricia <kvcspt0@gmail.com>
2024-12-07 09:48:18 +0100

    downgrade to angular 16 and add cornerstone viewer

--- src/app/dicom-viewer/dicom-viewer.component.ts
+++ src/app/dicom-viewer/dicom-viewer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ScanService } from '../services/scan.service';
 import { Scan } from '../models/scan';
+import { ImageInfo, RequestSchema, ToolEnum } from 'ng-cornerstone';
+import { Enums } from '@cornerstonejs/core';
 
 @Component({
   selector: 'app-dicom-viewer',
@@ -8,12 +10,42 @@ import { Scan } from '../models/scan';
 })
 export class DicomViewerComponent implements OnInit {
   public scans: Scan[] | undefined;
+  public toolList = [
+    ToolEnum.StackScrollTool,
+    ToolEnum.PanTool,
+    ToolEnum.ZoomTool,
+    ToolEnum.WindowLevelTool,
+    ToolEnum.LengthTool,
+    ToolEnum.AngleTool,
+    ToolEnum.RectangleROITool,
+    ToolEnum.EllipticalROITool,
+    ToolEnum.TrackballRotateTool,
+    ToolEnum.Rotate,
+    ToolEnum.Previous,
+    ToolEnum.Next,
+    ToolEnum.FlipV,
+    ToolEnum.FlipH,
+    ToolEnum.Reset,
+    ToolEnum.Axial,
+    ToolEnum.Sagittal,
+    ToolEnum.Coronal,
+  ];
+  public imageInfos: ImageInfo[] = [];
   public constructor(private scanService: ScanService) {}
 
   public ngOnInit(): void {
     this.scanService.scans$.subscribe({
       next: (data) => {
         this.scans = data;
+        this.imageInfos = [
+          {
+            studyInstanceUID: this.scans[0].studyUid,
+            seriesInstanceUID: this.scans[0].seriesUid,
+            urlRoot: 'http://localhost:8080/dicom-web',
+            viewportType: Enums.ViewportType.STACK,
+            schema: RequestSchema.wadoRs,
+          },
+        ];
       },
     });
   }
