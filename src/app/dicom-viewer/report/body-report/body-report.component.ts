9ef7d93848563f7dceb5ba612c444f18848ad90e
Patricia <kvcspt0@gmail.com>
2025-02-27 10:44:22 +0100

    Feat: list and show saved body templates

--- src/app/dicom-viewer/report/body-report/body-report.component.ts
+++ src/app/dicom-viewer/report/body-report/body-report.component.ts
@@ -1,15 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Type } from '@angular/core';
+import { BodyTemplate } from '../../../models/template';
+import { BodyService } from '../../../services/body/body.service';
+import { ToastrService } from 'ngx-toastr';
+import { KneeComponent } from './body-components/knee/knee.component';
+import { AbdomenComponent } from './body-components/abdomen/abdomen.component';
+import { ChestComponent } from './body-components/chest/chest.component';
+import { LiverComponent } from './body-components/liver/liver.component';
 
 @Component({
   selector: 'app-body-report',
   templateUrl: './body-report.component.html',
 })
-export class BodyReportComponent {
+export class BodyReportComponent implements OnInit {
   public templateFiles = [
     'knee.html',
     'liver.html',
     'chest.html',
     'abdomen.html',
   ];
+  public savedTemplates: BodyTemplate[] = [];
   public selectedBodyTemplate: string | undefined;
+  public selectedComponent: Type<any> | null = null;
+  public selectedBodyTemplateObject: BodyTemplate | null = null;
+  private templateComponentMap: { [key: string]: Type<any> } = {
+    'knee.html': KneeComponent,
+    'abdomen.html': AbdomenComponent,
+    'chest.html': ChestComponent,
+    'liver.html': LiverComponent,
+  };
+
+  public constructor(
+    private bodyService: BodyService,
+    private toastr: ToastrService,
+  ) {}
+  public ngOnInit(): void {
+    this.bodyService.getBodyTemplates().subscribe({
+      next: (value) => {
+        this.savedTemplates = value;
+
+        this.templateFiles = [
+          'knee.html',
+          'liver.html',
+          'chest.html',
+          'abdomen.html',
+          ...this.savedTemplates.map((template) => template.title + '.html'),
+        ];
+      },
+      error: (err) =>
+        this.toastr.error('Failed to load templates: ' + err.error),
+    });
+  }
+
+  public onTemplateChange(): void {
+    if (this.templateComponentMap[this.selectedBodyTemplate || '']) {
+      this.selectedComponent =
+        this.templateComponentMap[this.selectedBodyTemplate || ''];
+      this.selectedBodyTemplateObject = null;
+    } else {
+      this.selectedComponent = null;
+
+      const foundTemplate = this.savedTemplates.find(
+        (template) => template.title + '.html' === this.selectedBodyTemplate,
+      );
+
+      this.selectedBodyTemplateObject = foundTemplate
+        ? { ...foundTemplate }
+        : null;
+    }
+  }
 }
